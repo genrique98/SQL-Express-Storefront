db2f20016c16a63ad976176beac40451a00fef2b
gabriel <15620759+genrique98@users.noreply.github.com>
2021-07-06 21:10:11 -0700

    feat: added server file

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+// import { defaultMaxListeners } from 'events'
+import users_routes from './handlers/users.js';
 const app = express();
-const port = 3000;
+const port = 10533; //3000;
 app.use(bodyParser.json());
 app.get('/', function (_req, res) {
     res.send('Hello World!');
 });
+users_routes(app);
 app.listen(port, function () {
     console.log(`server started at http://localhost:${port}`);
 });
 export default app;
+// https://www.npmjs.com/package/cors
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
-import { defaultMaxListeners } from 'events'
+// import { defaultMaxListeners } from 'events'
+import users_routes from './handlers/users.js';
 
 const app: express.Application = express()
-const port = 3000;
+const port = 10533 //3000;
 
 app.use(bodyParser.json())
 
@@ -11,8 +12,14 @@ app.get('/', function (_req: Request, res: Response) {
     res.send('Hello World!')
 })
 
+users_routes(app)
+
+
 app.listen(port, function () {
     console.log(`server started at http://localhost:${port}`)
 })
 
 export default app;
+
+
+// https://www.npmjs.com/package/cors
