42b56869f0f2812ff5d3268937ac7d87a01ad9b7
gabriel <15620759+genrique98@users.noreply.github.com>
2021-08-26 23:15:57 -0700

    fix: database config fixed

--- dist/database.js
+++ dist/database.js
@@ -8,31 +8,11 @@ var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 var Pool = pg_1.default.Pool;
 var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_TEST_DB = _a.POSTGRES_TEST_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV;
-// let config = {};
 var config = {
     host: POSTGRES_HOST,
-    database: POSTGRES_TEST_DB,
+    database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD
 };
 var Client = new Pool(config);
-// if (ENV === 'test') {
-//     config = {
-//         host: POSTGRES_HOST,
-//         database: POSTGRES_TEST_DB,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD
-//     }
-//     Client = new Pool(config);
-// }
-// if (ENV === 'dev') {
-//     config = {
-//         host: POSTGRES_HOST,
-//         database: POSTGRES_DB,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD
-//     }
-//     Client = new Pool(config);
-// }
-// let Client = new Pool(config);
 exports.default = Client;
--- src/database.ts
+++ src/database.ts
@@ -10,42 +10,17 @@ const {
     POSTGRES_DB, 
     POSTGRES_TEST_DB, 
     POSTGRES_USER, 
-    POSTGRES_PASSWORD, 
+    POSTGRES_PASSWORD,
     ENV 
 } = process.env;
 
-// let config = {};
 const config = {
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
+    database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD
 }
 
 const Client = new Pool(config);
-
-
-// if (ENV === 'test') {
-//     config = {
-//         host: POSTGRES_HOST,
-//         database: POSTGRES_TEST_DB,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD
-//     }
-//     Client = new Pool(config);
-
-// }
-// if (ENV === 'dev') {
-//     config = {
-//         host: POSTGRES_HOST,
-//         database: POSTGRES_DB,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD
-//     }
-//     Client = new Pool(config);
-
-// }
-
-// let Client = new Pool(config);
 
 export default Client
