66f309c4ee2680e2a7429ae34764b730fb64b8c4
gabriel <15620759+genrique98@users.noreply.github.com>
2021-08-29 20:01:05 -0700

    fix: table columns

--- migrations/sqls/20210704040127-order-products-table-up.sql
+++ migrations/sqls/20210704040127-order-products-table-up.sql
@@ -3,6 +3,5 @@
 CREATE TABLE order_products (
     id SERIAL PRIMARY KEY,
     quantity INTEGER,
-    order_id bigint REFERENCES orders(id),
-    product_id bigint REFERENCES products(id)
+    order_id INTEGER REFERENCES orders(id)
 );
