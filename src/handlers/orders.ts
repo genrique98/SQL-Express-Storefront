8dacaa793e4481bf31d4d990ca85542f9891f8ba
gabriel <15620759+genrique98@users.noreply.github.com>
2021-08-29 20:00:16 -0700

    fix: orders route

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -5,24 +5,22 @@ import verifyAuthToken from '../middleware/auth'
 
 const store = new OrderStore()
 
-// const index = async (_req: Request, res: Response) => {
-//     try {
-//         const orders = await store.index()
-//         res.json(orders)
-//     } catch (err) {
-//         console.log(err)
-//     }
-    
-// }
-
-const show = async (req: Request, res: Response): Promise<void> => {
+const index = async (_req: Request, res: Response) => {
     try {
-        const users = await store.show(req.body.id)
-        res.json(users)
+        const orders = await store.index()
+        res.json(orders)
     } catch (err) {
         console.log(err)
     }
-    
+}
+
+const show = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const order = await store.show(req.params.id)
+        res.json(order)
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 const create = async (req: Request, res: Response): Promise<void> => {
@@ -33,23 +31,19 @@ const create = async (req: Request, res: Response): Promise<void> => {
     const { TOKEN_SECRET } = process.env;
     try {   
         const newOrder = await store.create(order)
-        var token = jwt.sign({ user: newOrder }, TOKEN_SECRET as Secret);
+        let token = jwt.sign({ order: newOrder }, TOKEN_SECRET as Secret);
         res.json(token)
     } catch (err) {
         console.log(err)
     }
-    
 }
 
 const addProduct = async (req: Request, res: Response): Promise<void> => {
     let request = req.body;
-
     const order_product: Order_product = {
         quantity: request.quantity,
-        orderId: request.orderId,
-        productId: request.productId,
+        orderId: parseInt( (req.params.id as unknown) as string), // req.body.orderId,
     }
-  
     try {
       const addedProduct = await store.addProduct(order_product)
       res.json(addedProduct)
@@ -59,32 +53,12 @@ const addProduct = async (req: Request, res: Response): Promise<void> => {
     }
   } 
 
-// const authenticate = async (req: Request, res: Response) => {
-//     const user: User = {
-//         firstName: req.body.firstName,
-//         lastName: req.body.lastName,
-//         username: req.body.username,
-//         password: req.body.password,
-//     }
-//     const { TOKEN_SECRET } = process.env;
-//     try {
-//         const oldUser = await store.authenticate(user.username, user.password)
-//         var token = jwt.sign({ user: oldUser }, TOKEN_SECRET as Secret);
-//         res.json(token)
-//     } catch(error) {
-//         res.status(401)
-//         res.json({ error })
-//     }
-//   }
-
-
 const order_routes = (app: express.Router): void => {
-    // app.get('/orders', verifyAuthToken, index)
-    app.get('/orders/:id', verifyAuthToken, show) 
-    app.post('/orders', verifyAuthToken, create) // is this like add product?
-    
-    // add product to cart
-    app.post('/orders/:id/products', verifyAuthToken, addProduct)
+    app.get('/orders', verifyAuthToken, index) // show all orders (admin auth)
+    app.get('/orders/users/:id', verifyAuthToken, show) // show current cart of user -> complete data shape
+    app.post('/orders', verifyAuthToken, create) // create new order
+    app.post('/orders/:id/products', verifyAuthToken, addProduct) // add product to cart(specific order)
+
 
 }
 
