92f810e7c51803ba39eba3d0b2b3db1e83fa924e
gabriel <15620759+genrique98@users.noreply.github.com>
2021-08-31 18:16:39 -0700

    fix: connection test

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,20 +1,17 @@
 import Client from '../database'
-import { PoolClient } from 'pg'
 
 // test connection to database
-export const connection = (): void => {
+export const connection = async (): Promise<void> => {
     let retries = 5;
     while (retries) {
-        async () => {
-            try {
-                await Client.connect();
-            } catch (err) {
-                console.log(err);
-                retries -= 1;
-                console.log(`retries left: ${retries}`);
-                // wait 5 seconds
-                await new Promise(res => setTimeout(res, 1000));
-            }
+        try {
+            await Client.connect();
+        } catch (err) {
+            console.log(err);
+            retries -= 1;
+            console.log(`retries left: ${retries}`);
+            // wait 5 seconds
+            await new Promise(res => setTimeout(res, 1000));
         }
     }
 }
