807f3d3c9cf20d6578bdfe9e6c4143341ff9c6f2
gabriel <15620759+genrique98@users.noreply.github.com>
2021-09-06 22:25:20 -0700

    fix: table foreign key references

--- migrations/sqls/20210704040127-order-products-table-up.sql
+++ migrations/sqls/20210704040127-order-products-table-up.sql
@@ -1,7 +1,9 @@
 /* Replace with your SQL commands */
 
 CREATE TABLE order_products (
-    id SERIAL PRIMARY KEY,
+    product_id INTEGER,
     quantity INTEGER,
-    order_id INTEGER REFERENCES orders(id)
+    order_id INTEGER,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (order_id) REFERENCES orders(id)
 );
